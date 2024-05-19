773a667a6eed3b459160e973ccfb3817a44c1c82
Suryasmaty <teja.sadanala@gmail.com>
2024-05-19 14:01:02 +0530

    added apollo server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
-import cors from 'cors';
-import express from 'express';
-import { authMiddleware, handleLogin } from './auth.js';
+import cors from "cors";
+import express from "express";
+import { authMiddleware, handleLogin } from "./auth.js";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware as apolloMiddleware } from "@apollo/server/express4";
 
 const PORT = 9000;
 
 const app = express();
 app.use(cors(), express.json(), authMiddleware);
 
-app.post('/login', handleLogin);
+app.post("/login", handleLogin);
+
+const apolloServer = new ApolloServer({
+  /*typeDefs, resolvers*/
+});
+
+await apolloServer.start();
+app.use("/graphql", apolloMiddleware(apolloServer));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
